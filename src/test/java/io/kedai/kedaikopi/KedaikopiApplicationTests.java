6de94fe920be22feecbd7e48b77a9727d14928c5
iniudin <sysfdn@gmail.com>
2023-04-07 04:28:17 +0700

    Bismillah, memulai project

---
+++ src/test/java/io/kedai/kedaikopi/KedaikopiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kedai.kedaikopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedaikopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
